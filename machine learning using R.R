b9375a1bdab1735e905e5fe66be403117fce4728
alecfei <2022173@student.cct.ie>
2023-03-14 11:19:17 +0000

    machine learning

--- machine learning using R.R
+++ machine learning using R.R
@@ -6,3 +6,15 @@ mushroom_ml <- read.csv("gilled mushrooms.csv", sep = ",")
 
 # View dataset
 View(mushroom_ml)
+
+# Remove distinct column veil.type
+
+##library(dplyr)
+##mushroom_ml %>% select(-veil.type)
+
+mushroom_ml$veil.type <- NULL
+View(mushroom_ml)
+
+# Label Encoding categorical variables
+mushroom_ml$cap.shape <- as.numeric(factor(mushroom_ml$cap.shape))
+mushroom_ml
